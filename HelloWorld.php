c0f5ac15501d4ec6984ca75396e929d352786f80
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-05 16:28:59 -0400

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<title>Index</title>
+<title>Hello World</title>
 </head>
 <body>
 
