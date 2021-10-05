783a37f307b8cd78a1cd1b3bd073155fc2cdf7b2
TheHobnobler <53322601+TheHobnobler@users.noreply.github.com>
2021-10-05 09:16:20 -0400

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -20,6 +20,7 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="FAQ.php">FAQ</a>
+      </li>  
       <li class="nav-item">
         <a class="nav-link" href="retrievetime.php">Get Timestamp</a> 
       </li>
