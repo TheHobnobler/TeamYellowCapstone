21d42f14ff2f4f7e23b5d2a6805aa475b9767e10
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:03:48 -0400

    Update ContactUs.php

--- ContactUs.php
+++ ContactUs.php
@@ -1,7 +1,36 @@
-<?php
-include 'functions.php';
-?>
-<?=template_header('Contact Us')?>
+    <!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<title>$title</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="HomePage.php">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="HelloWorld.php">Hello World</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ContactUs.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FAQ.php">FAQ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="retrievetime.php">Get Timestamp</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="TestCart.php">Test Cart</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 <div class="container">
     <h1>Contact Us</h1>
