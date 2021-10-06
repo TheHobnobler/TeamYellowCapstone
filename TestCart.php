510d0137ec70f0c93154a7193715d4d47f26e975
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:10:36 -0400

    Update TestCart.php

--- TestCart.php
+++ TestCart.php
@@ -1,7 +1,16 @@
+    <!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<title>TESTCART</title>
+</head>
+<body>
+    
 <?php
 include 'functions.php';
 ?>
-<?=template_header('Test Cart')?>
+<?=navbar()?>
 
 <?php
 function addToCart(){
