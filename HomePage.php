e4a01a813becade2967b327cfdb27ebc67e98ad4
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-05 23:57:16 -0400

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<title>Love You a Latte</title>
-</head>
-<body>
-
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <div class="container-fluid">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="HomePage.php">Home</a>
-      </li>
-     <li class="nav-item">
-        <a class="nav-link" href="HelloWorld.php">Hello World</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="ContactUs.php">Contact Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="FAQ.php">FAQ</a>
-      </li>  
-      <li class="nav-item">
-        <a class="nav-link" href="retrievetime.php">Get Timestamp</a> 
-      </li>
-    </ul>
-  </div>
-</nav>
+<?php
+include 'functions.php';
+?>
+<?=template_header('Love You A Latte')?>
 
 <div class="container">
     <h1>Welcome to Love You a Latte</h1>
