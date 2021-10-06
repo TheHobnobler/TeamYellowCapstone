4f8b079e2b0390c17bc5a31828fa79977a540c3a
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:30:59 -0400

    Update Functions.php

--- Functions.php
+++ Functions.php
@@ -1,7 +1,15 @@
 <?php
 
-function navbar() {
+function navbar($title) {
     echo <<<EOT
+    <!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<title>$title</title>
+</head>
+<body>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
