66604eb2e87180ce6c82eb3b0eba47fe87303ae7
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:36:56 -0400

    Update retrievetime.php

--- retrievetime.php
+++ retrievetime.php
@@ -1,11 +1,7 @@
-    <!DOCTYPE html>
-<html>
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-<title>Retrieve The Time</title>
-</head>
-<body>
+<?php
+include 'functions.php';
+?>
+<?=navbar('Retrieve the Time')?>
     
 <?php
 include 'functions.php';
