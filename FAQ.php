7bb9dc402efacc9adab7de360a50a8fc140ccbf8
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:09:52 -0400

    Update FAQ.php

--- FAQ.php
+++ FAQ.php
@@ -1,7 +1,16 @@
+    <!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<title>FAQ</title>
+</head>
+<body>
+  
 <?php
 include 'functions.php';
 ?>
-<?=template_header('FAQ')?>
+<?=navbar()?>
 
 <div class="container">
   
