4ec9a61ba058d903107021d20643dc499ff4de07
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:09:16 -0400

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -1,7 +1,16 @@
+    <!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+<title>Love You A Latte</title>
+</head>
+<body>
+    
 <?php
 include 'functions.php';
 ?>
-<?=template_header('Love You A Latte')?>
+<?=navbar()?>
 
 <div class="container">
     <h1>Welcome to Love You a Latte</h1>
