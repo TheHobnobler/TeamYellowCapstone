7844f8f630d0685d66cea492ce3c9872a5f3ebfe
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:25:04 -0400

    Update ContactUs.php

--- ContactUs.php
+++ ContactUs.php
@@ -1,14 +1,15 @@
+    <?php
+include 'functions.php';
+?> 
     <!DOCTYPE html>
 <html>
 <head>
-    <?php
-include 'functions.php';
-?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
-<title>Contact Us</title>
+<title>$title</title>
 </head>
 <body>
+
 <?=navbar()?>
 
 <div class="container">
