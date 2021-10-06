4241d53e7d24f5cd7e911164f0c0e8c1271798f9
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-06 00:06:38 -0400

    Update ContactUs.php

--- ContactUs.php
+++ ContactUs.php
@@ -6,31 +6,7 @@
 <title>$title</title>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <div class="container-fluid">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="HomePage.php">Home</a>
-      </li>
-     <li class="nav-item">
-        <a class="nav-link" href="HelloWorld.php">Hello World</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="ContactUs.php">Contact Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="FAQ.php">FAQ</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="retrievetime.php">Get Timestamp</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="TestCart.php">Test Cart</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<?=navbar()?>
 
 <div class="container">
     <h1>Contact Us</h1>
