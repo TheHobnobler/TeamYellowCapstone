8f3557d0c177b9c4727c0f81d01f8e58426ba9f2
TheHobnobler <53322601+TheHobnobler@users.noreply.github.com>
2021-10-11 19:21:33 -0400

    Update TestCart.php

--- TestCart.php
+++ TestCart.php
@@ -67,9 +67,9 @@ $cart = array(array("","",""));
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
   </div>
   <div class="offcanvas-body">
-    <?php
-    echo $shopcart[0][2];
-     ?>
+    <p>Some text lorem ipsum.</p>
+    <p>Some text lorem ipsum.</p>
+    <p>Some text lorem ipsum.</p>
     <button class="btn btn-secondary" type="button">A Button</button>
   </div>
 </div>
