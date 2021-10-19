b898857be8dab485b93c28631be06284b663eb74
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-19 18:38:34 -0400

    Update clearCart.php

--- clearCart.php
+++ clearCart.php
@@ -1,23 +1,11 @@
 <?php
 session_start();
 
-$i=1;
+$i=0;
 while($i< count($_SESSION["cart"])){
     array_pop($_SESSION["cart"]);
-}
+} 
+  echo "<h4>You have 0 item(s) in your cart.</h4><h4>Total $0";
 
-
-$total = 0;
-for($i=1; $i <count($_SESSION["cart"]); $i++){
-    $total += $_SESSION["cart"][$i][2];
-  }
-
-$count =  count($_SESSION["cart"])-1;
-for($i=1; $i < count($_SESSION["cart"]); $i++){
-    echo 
-    "<br>" .$_SESSION["cart"][$i][1]. " " .$_SESSION["cart"][$i][0]. " for : $" .$_SESSION["cart"][$i][2]. "</br>";
-  }
-  
-  echo "<h4>You have {$count} item(s) in your cart.</h4><h4>Total $ {$total}";
 
 ?>
