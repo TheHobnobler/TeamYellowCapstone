f26314085ae3b4cd61ea575677537e46681d3533
VadimZhura <90975523+VadimZhura@users.noreply.github.com>
2021-11-01 20:57:23 -0400

    Add files via upload
    Log in page hopefully

---
+++ Log-In.php
@@ -0,0 +1,72 @@
+<?php 
+
+$host="localhost";
+$user="root";
+$password="";
+$db="demo";
+
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['username'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
+    
+    $result=mysql_query($sql);
+    
+    if(mysql_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Log In</title>
+        <link rel="" a href=""/>
+    
+    </head>
+    
+<body> 
+    
+    <form>
+        <div class="form-input">
+            <input type="text" name="username" placeholder="Enter your User Name">
+        </div>
+        
+        <div class="form-input">
+             <input type="password" name="password" placeholder="Enter your Passsword">
+        </div>
+        
+        
+    </form>
+    
+    
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
