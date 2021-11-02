d678b7e6cfa8751dc9bc2afbb0a746bc39bf5ae8
TheHobnobler <53322601+TheHobnobler@users.noreply.github.com>
2021-11-01 21:24:53 -0400

    Update menudb.php

--- menudb.php
+++ menudb.php
@@ -24,19 +24,22 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 $sql_retrieve = "SELECT * FROM menu_tbl";
 $result = mysqli_query($conn, $sql_retrieve);
     
-if (!$result) {
-    $message .= 'Whole query: ' .$query;
-    die($message);
-}
+if ($result = $mysqli->query($sql_retrieve)) {
 
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "Menu" . $row["ts"].  "<br>";
-  }
-} else {
-  echo "0 results";
-}
+    while ($row = $result->fetch_assoc()) {
+        $SKU = $row["col1"];
+        $ITEM = $row["col2"];
+        $DESCRIPTION = $row["col3"];
+        $PRICE = $row["col4"];
+        $SIZE = $row["col5"];
+
+        echo $SKU;
+        echo $ITEM;
+        echo $DESCRIPTION;
+        echo $PRICE;
+        echo $SIZE;
+    }
+
 
 mysqli_close($conn);
 }
