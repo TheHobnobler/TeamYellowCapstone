8f5df78c6468ab62387b703f34a1e6b50e57cc65
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-06 21:45:54 -0400

    Update Log-In.php

--- Log-In.php
+++ Log-In.php
@@ -87,5 +87,19 @@ if ($result->num_rows > 0) {
 ?> 
   -->
   </body>
+
+  <script>
+
+function employeeLogin(str) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("returnBox").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "employeeLogin.php?q=" + str, true);
+    xmlhttp.send();
+  }
+</script>
     
 </html>
