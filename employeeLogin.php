617613c14e42045bdfdd0794aa9854b02113e404
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-07 00:28:46 -0400

    Update employeeLogin.php

--- employeeLogin.php
+++ employeeLogin.php
@@ -8,27 +8,9 @@ session_start();
 
 echo $_SESSION["count"];
 $_SESSION["count"] ++;
-/*
-$servername = "localhost";
-$username = "root";
-$password = "teamyellow";
-$db = "coffee_emps";
 
-$conn = new mysqli($servername, $username, $password, $db);
- 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} <?php 
 
-session_start();
 
-//$q = $_REQUEST["q"];
-
-//echo $q;
-
-echo $_SESSION["count"];
-$_SESSION["count"] ++;
-/*
 $servername = "localhost";
 $username = "root";
 $password = "teamyellow";
@@ -51,69 +33,7 @@ if ($result->num_rows > 0) {
   echo "0 results";
 }
 
-*/
 
-$serverName = "aatjxyjvs2g8ic.cayunuubrbla.us-east-1.rds.amazonaws.com, 1433"; //serverName\instanceName, portNumber (default is 1433)
-$connectionInfo = array( "Database"=>"LoveYouALatte", "UID"=>"root", "PWD"=>"Capstone2021!");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}
-  ?>
-  
-$sql = "SELECT * FROM loginfrom";
-$result = mysqli_query($conn, $sql);
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo $row["User"]. $row["Pass"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
 
-*/
-
-$host ="aatjxyjvs2g8ic.cayunuubrbla.us-east-1.rds.amazonaws.com, 1433"; 
-$username ="root";
-$password ="Capstone2021!";
-$database ="LoveYouALatte";
-
-mssql_connect($host, $username, $password);
-mssql_select_db($database);
-
-$query ="SELECT * FROM Coffee";
-$result =mssql_query($query);
-while ( $record = mssql_fetch_array($result) )
-{
-	echo $record["coffee"] .", ". $record["size"] ."<br />";
-}
-
-/*
-try
-        {
-
-    if($conn == false)
-        die(FormatErrors(sqlsrv_errors()));
-            echo "connected";
-            $tsql = "SELECT * FROM Coffee";
-            $result = sqlsrv_query($conn, $tsql);
-            if ($results == FALSE)
-              die(FormatErrors(sqlsrv_errors()));
-            while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-              echo $row["coffee"]. " " .$row["size"]. "<br>";
-          }
-          sqlsrv_free_stmt($results);
-            sqlsrv_close($conn);
-        }
-        catch(Exception $e)
-        {
-            echo("Error!");
-        }
-  
-*/
   ?>
