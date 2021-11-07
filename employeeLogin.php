e1a7496b95baa57266bb6f977ab62d6a2b563402
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-06 23:10:38 -0400

    Update employeeLogin.php

--- employeeLogin.php
+++ employeeLogin.php
@@ -8,7 +8,7 @@ session_start();
 
 echo $_SESSION["count"];
 $_SESSION["count"] ++;
-
+/*
 $servername = "localhost";
 $username = "root";
 $password = "teamyellow";
@@ -26,6 +26,33 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo $row["User"]. $row["Pass"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+*/
+
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "Capstone2021!";
+$db = "LoveYouALatte";
+
+$conn = new mysqli($servername, $username, $password, $db);
+ 
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+  
+$sql = "SELECT * FROM Coffee";
+$result = mysqli_query($conn, $sql);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo $row["coffee"]. $row["size"]. "<br>";
   }
 } else {
   echo "0 results";
