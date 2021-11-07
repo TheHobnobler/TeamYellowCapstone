eaab89bb49e4d3c8624e07240f71b0e13c44d918
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-07 00:14:17 -0400

    Update Log-In.php

--- Log-In.php
+++ Log-In.php
@@ -1,57 +1,46 @@
-<?php
-include 'Functions.php';
+<?php 
+
 session_start();
-$_SESSION["count"];
 
-?>
+//$q = $_REQUEST["q"];
 
+//echo $q;
 
+echo $_SESSION["count"];
+$_SESSION["count"] ++;
+/*
+$servername = "localhost";
+$username = "root";
+$password = "teamyellow";
+$db = "coffee_emps";
 
-<?=navbar('Login')?>
-
-<div class='container'>
-
-
-
-    
-    <formmethod="post" action="#">
-        <div class="form-input">
-            <input type="text" name="username" placeholder="Enter your User Name">
-        </div>
-        
-        <div class="form-input">
-             <input type="password" name="password" placeholder="Enter your Passsword">
-        </div>
-        
-        <input onclick='employeeLogin("foo")' type="submit" type="submit" value="LOGIN" class="btn-login">
-        
-    </form>
-
-
-    <form action=''>
-            <label for='fname'></label>
-            <input type='button' value ='employeeLogin' id='' name='foo' onclick="employeeLogin(document.getElementById('returnBox').innerHTML)">
-          </form>
-    
-    <span id='returnBox'></span>
-
-</div>
-
-
-  </body>
-
-  <script>
-
-function employeeLogin(str) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("returnBox").innerHTML = this.responseText;
-      }
-    };
-    xmlhttp.open("GET", "employeeLogin.php?q=" + str, true);
-    xmlhttp.send();
+$conn = new mysqli($servername, $username, $password, $db);
+ 
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+  
+$sql = "SELECT * FROM loginfrom";
+$result = mysqli_query($conn, $sql);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo $row["User"]. $row["Pass"]. "<br>";
   }
-</script>
-    
-</html>
+} else {
+  echo "0 results";
+}
+
+*/
+
+$serverName = "aatjxyjvs2g8ic.cayunuubrbla.us-east-1.rds.amazonaws.com, 1433"; //serverName\instanceName, portNumber (default is 1433)
+$connectionInfo = array( "Database"=>"LoveYouALatte", "UID"=>"root", "PWD"=>"Capstone2021!");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+  ?>
