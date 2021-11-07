b07a027b41f71b1f7cdd52794cf3d45ba8a44140
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-06 22:06:30 -0400

    Update Log-In.php

--- Log-In.php
+++ Log-In.php
@@ -2,7 +2,7 @@
 include 'Functions.php';
 session_start();
 
-$_SESSION["foo"] ="foo"; 
+
 ?>
 
 
@@ -33,61 +33,11 @@ $_SESSION["foo"] ="foo";
             <input type='button' value ='employeeLogin' id='' name='foo' onclick="employeeLogin(document.getElementById('returnBox').innerHTML)">
           </form>
     
-    <span id='returnBox'>foo</span>
+    <span id='returnBox'></span>
 
 </div>
 
-<?php 
 
-$servername = "localhost";
-$username = "root";
-$password = "teamyellow";
-$db = "coffee_emps";
-
-$conn = new mysqli($servername, $username, $password, $db);
- 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} 
-  
-$sql = "SELECT * FROM loginfrom";
-$result = mysqli_query($conn, $sql);
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo $row["User"]. $row["Pass"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-  ?>
-
-
-
-
-<!---
-  
- if(isset($_POST['username'])){
-    
-    $uname=$_POST['username'];
-    $password=$_POST['password'];
-    
-    $sql="select * from loginfrom where user='".$uname."'AND Pass='".$password."' limit 1";
-    
-    $result=mysql_query($sql);
-    
-    if(mysql_num_rows($result)==1){
-        echo " You Have Successfully Logged in";
-        exit();
-    }
-    else{
-        echo " You Have Entered Incorrect Password";
-        exit();
-    }
-        
-}
-?> 
-  -->
   </body>
 
   <script>
