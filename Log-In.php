da0590b9afc2cda84d1e46f92f3aa79ac3323542
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-07 00:16:40 -0400

    Update Log-In.php

--- Log-In.php
+++ Log-In.php
@@ -34,7 +34,18 @@ $_SESSION["count"];
           </form>
     
     <span id='returnBox'></span>
+    <?php
+    $serverName = "aatjxyjvs2g8ic.cayunuubrbla.us-east-1.rds.amazonaws.com, 1433"; //serverName\instanceName, portNumber (default is 1433)
+$connectionInfo = array( "Database"=>"LoveYouALatte", "UID"=>"root", "PWD"=>"Capstone2021!");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+  ?>
 </div>
 
 
