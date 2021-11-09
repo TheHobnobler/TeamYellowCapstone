1647cae924c4a9501057d2ccce386ca2d630e3df
tristanweary <90973996+tristanweary@users.noreply.github.com>
2021-11-08 21:14:07 -0500

    Add files via upload

---
+++ LoginSuccessfull.php
@@ -0,0 +1,13 @@
+<DOCTYPE! html>
+<html>
+<head>
+<title>Welcome to the Employee Page</title>
+</head>
+<body>
+
+
+<?php echo "Welcome to the employee login page " . $_SESSION["Username"]>
+
+</body>
+
+</html>
