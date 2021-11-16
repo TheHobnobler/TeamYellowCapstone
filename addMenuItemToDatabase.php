535b22bfa2708c7e55f68bca5790d4b9b192b518
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-15 22:12:03 -0500

    Add files via upload

---
+++ addMenuItemToDatabase.php
@@ -0,0 +1,27 @@
+<?php
+echo "foo";
+
+$q = $_REQUEST["q"];
+
+$sql = $q;
+
+$servername =  "localhost";
+$username  = "root";
+$password = "teamyellow";
+$dbname = "coffeeshop";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
+
+
+
+?>
