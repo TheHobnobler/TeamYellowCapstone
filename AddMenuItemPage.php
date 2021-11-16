763e24f8c81ad90d986202940b12f58aa80167be
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-15 22:31:13 -0500

    Update AddMenuItemPage.php

--- AddMenuItemPage.php
+++ AddMenuItemPage.php
@@ -95,7 +95,7 @@ echo $size;
 echo "<br>";
 
 $sql = "INSERT INTO menu_tbl (item, description, price, size)
-VALUES ('{$name}', '{$desc}', '{$price}', '{$size}';)";
+VALUES ('{$name}', '{$desc}', '{$price}', '{$size}');";
 echo $sql;
 ?>
 </span>
