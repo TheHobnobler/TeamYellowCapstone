f53cdad2c6db3799b898be4651526fa7df34422e
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-15 22:13:22 -0500

    Update AddMenuItemPage.php

--- AddMenuItemPage.php
+++ AddMenuItemPage.php
@@ -2,41 +2,109 @@
 include 'Functions.php';
 ?>
 <?=navbar('Add Menu TEST')?>
+<?php
+// define variables and set to empty values
+$skuErr = $nameErr = $descErr = $priceErr = $sizeErr= "";
+$sku = $name = $desc = $price = $size = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["sku"])) {
+    $skuErr = "Product SKU is required";
+  } else {
+    $sku = test_input($_POST["sku"]);
+  }
+  
+  if (empty($_POST["name"])) {
+    $nameErr = "Product name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+    
+  if (empty($_POST["desc"])) {
+    $desc = "";
+  } else {
+    $desc = test_input($_POST["desc"]);
+  }
+
+  if (empty($_POST["price"])) {
+    $priceErr = "Product price is required";
+  } else {
+    $price = test_input($_POST["price"]);
+  }
+
+  if (empty($_POST["size"])) {
+    $sizeErr = "Product size is required";
+  } else {
+    $size = test_input($_POST["size"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
 
 <div class="container">
-    <h1>Welcome to Love You a Latte</h1>
-    <p>This is a work in progress for a home landing page.</p>
+    <h1>Item Activation Portal</h1>
+    <p>This is a page to enter new items to the menu.</p>
 
 
-<form>
-    <ul>
-    <li><label for="fname">SKU :</label></li>
-    <li><input type="text" id="SKU" name="fname"></li>
-    <li> <p class="rq">*Required Field</p></li>
-    
-    <li><label for="fname">Item Name:</label></li>
-    <li><input type="text" id="itemName" name="fname"></li>
-    <li><p class="rq">*Required Field</p></li>
 
-    <li><label for="fname">Item Description:</label></li>
-    <li><input type="text" id="itemDesc" name="fname"></li>
-    <li><p class="rq">*Required Field</p></li>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+<ul>
+  
+  <li> Product Name: <li><input type="text" name="name"></li></li>
+  <li><span class="error">* <?php echo $nameErr;?></span></li>
+  <br>
 
-    <li><label for="fname">Item Price:</label></li>
-    <li><input type="text" id="itemPrice" name="fname"></li>
-    <li><p class="rq">*Required Field</p></li>
+  <li>Product Description: <li><input type="text" name="desc"></li></li>
+  <li><span class="error"><?php echo $descErr;?></span></li>
+  <br>
 
-    <li><label for="fname">Item Size:</label></li>
-    <li><input type="text" id="itemSize" name="fname"></li>
-    <li><p class="rq">*Required Field</p></li>
+  <li> Product Price: <li><input type="text" name="price"></li></li>
+  <li><span class="error">* <?php echo $priceErr;?></span></li>
+  <br>
 
-   
-            
-    <li><input type='button' value ='Add To Menu' id='' name='' onclick='addMenuItemToDatabase(this.name)'></li>
+  <li> Product Size: <li><input type="text" name="size"></li></li>
+  <li><span class="error">* <?php echo $sizeErr;?></span></li>
+  
+
+  <li><input type="submit" name="submit" value="Submit"></li>  
 </ul>
- 
-
 </form>
+
+
+
+
+<span id='userInput'>
+<?php
+echo "<h2>Your Input:</h2>";
+echo $sku;
+echo "<br>";
+echo $name;
+echo "<br>";
+echo $desc;
+echo "<br>";
+echo $price;
+echo "<br>";
+echo $size;
+echo "<br>";
+
+$sql = "INSERT INTO menu_tbl (ITEM, DESCRIPTION, PRICE, SIZE)
+VALUES ('{$name}', '{$desc}', '{$price}', '{$size})";
+echo $sql;
+?>
+</span>
+
+<form action=''>
+        <label for='fname'>Are you ready to submit?</label>
+        <input type='button' value ='Submit' id='' name='Submit' onclick='addMenuItemToDatabase(<?php $sql ?>)'>
+</form>
+<span id='returnBox'></span>
 </div>
 
 </body>
@@ -45,10 +113,6 @@ include 'Functions.php';
 
 
  function addMenuItemToDatabase(str) {
-    if(){
-        
-    }
-
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -73,6 +137,7 @@ include 'Functions.php';
         color: #FF0000;
         font-size: 10pt;
 }
+.error {color: #FF0000;}
 </style>
 
 
