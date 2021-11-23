4dda374c2e92421bee79553f4daa189c0d255879
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-23 16:30:29 -0500

    Add files via upload

---
+++ searchItemByName.php
@@ -0,0 +1,33 @@
+<?php
+
+$q = $_REQUEST["q"];
+
+
+
+
+$sql = "select sku, item, description, price, size from menu_tbl where item = '{$q}'; ";
+
+$servername =  "localhost";
+$username  = "root";
+$password = "teamyellow";
+$dbname = "coffeeshop";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $result = $conn->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {   
+        echo "<option>" .$row["ITEM"]. "\t". "Size: " . $row["SIZE"]. "\t". "Price: " . $row["PRICE"]. "</option";
+    }
+  } else {
+    echo "0 results";
+  }
+
+        
