ff6871859a8f3e6e9dd05eacb2a1e351c1d17c0c
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-23 03:49:10 -0500

    Update searchItemById.php

--- searchItemById.php
+++ searchItemById.php
@@ -2,6 +2,8 @@
 
 $q = $_REQUEST["q"];
 
+echo $q;
+
 $sql = "select sku, item, description, price, size from menu_tbl where sku = '{$q}' ";
 
 $servername =  "localhost";
@@ -43,7 +45,27 @@ if ($conn->connect_error) {
       </form>";
     }
   } else {
-    echo "0 results";
+    echo "
+    <form method=''>
+      <ul>
+        
+      <li> Product SKU: <li><input class='form-control'  readonly id='name' type='text' name='name' value='0 results'></li></li>
+        <br>
+      
+        <li> Product Name: <li><input id='name' type='text' name='name' value=''></li></li>
+       <br>
+      
+        <li>Product Description: <li><input id='desc' type='text' name='desc' value =''></li></li>
+        <br>
+      
+        <li> Product Price: <li><input  id='price' type='text' name='price' value=''></li></li>
+        <br>
+      
+        <li> Product Size: <li><input id='size' type='text' name='size' value=''></li></li>
+        
+        <li><input type='button' name='submit' value='Submit'></li>  
+      </ul>
+      </form>";
   }
   $conn->close();
 
