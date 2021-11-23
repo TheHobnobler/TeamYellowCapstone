c55cdaa936a48bc7f1f1b0b331c0905af3100b12
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-23 06:04:12 -0500

    Update AddMenuItemPage.php

--- AddMenuItemPage.php
+++ AddMenuItemPage.php
@@ -94,14 +94,15 @@ echo "<br>";
 echo $size;
 echo "<br>";
 
-$sql = "insert into  menu_tbl (item, description, price, size) values ('{$name}', '{$desc}', '{$price}', '{$size}');";
+
+$sql = "'{$name}', '{$desc}', '{$price}', '{$size}'";
 echo "<span id='sql' name='" .$sql. "'></span>";
 ?>
 </span>
 
 <form action=''>
         <label for='fname'>Are you ready to submit?</label>
-        <input type='button' value ='Submit' id='' name='Submit' onclick='addMenuItemToDatabase("<?php echo $sql?>")'>
+        <input type='button' value ='Submit' id='' name='Submit' onclick="addMenuItemToDatabase('<?php echo $sql?>')">
 </form>
 <span id='returnBox'></span>
 </div>
