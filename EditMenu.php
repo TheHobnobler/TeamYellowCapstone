15e3b819fb1a0f8973cc1b0cdcfebe76a9a209a5
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-23 05:10:09 -0500

    Update EditMenu.php

--- EditMenu.php
+++ EditMenu.php
@@ -72,17 +72,7 @@ include 'Functions.php';
 
 <script>
 
-var input = document.getElementById("searchId");
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a clic
-  }
-});
 
  function searchItemByName(str) {
     var xmlhttp = new XMLHttpRequest();
