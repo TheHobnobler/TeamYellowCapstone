67b183c530ae547037a8d22eb191395f3e8cd8f8
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-23 16:06:24 -0500

    Add files via upload

---
+++ updateMenuItem.php
@@ -0,0 +1,41 @@
+<?php 
+
+
+
+
+$q = $_REQUEST["q"];
+
+echo $q;
+
+$array = explode(",",$q);
+$sku = $array[0];
+$name = $array[1];
+$desc = $array[2];
+$price = $array[3];
+$size = $array[4];
+
+
+$sql = "update menu_tbl set item='{$name}', description='{$desc}', price = '{$price}', size = '{$size}' where sku={$sku}";
+
+
+
+$servername =  "localhost";
+$username  = "root";
+$password = "teamyellow";
+$dbname = "coffeeshop";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
+
+
+
+?>
