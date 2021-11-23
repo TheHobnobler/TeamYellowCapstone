efae69912f41faf3d20d495ee89ee27baf0215ad
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-11-23 18:18:09 -0500

    Update searchItemByName.php

--- searchItemByName.php
+++ searchItemByName.php
@@ -23,7 +23,7 @@ if ($conn->connect_error) {
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {   
-        echo "<option value='{$row['sku']}'>{$row['item']} Price: {$row['price']} Size: {$row['size']}</option>";
+        echo "<option value='{$row['sku']}'>{$row['item']} Price: {$row['price']} Size: {$row['size']} -----SKU: {$row['sku']}</option>";
     }
     
   } else {
