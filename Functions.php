689e0a9689f9fb4c784f2e5ae41c069b537ac26b
TheHobnobler <53322601+TheHobnobler@users.noreply.github.com>
2021-12-02 08:00:46 -0500

    Update Functions.php

--- Functions.php
+++ Functions.php
@@ -38,7 +38,7 @@ function navbar($title) {
         <a class="nav-link" href="AddMenuItemPage.php">Add Menu Item</a>
       </li>
        <li class="nav-item">
-        <a class="nav-link" href="EditMenu.php">Add Menu Item</a>
+        <a class="nav-link" href="EditMenu.php">Edit Menu Item</a>
       </li>
     </ul>
   </div>
